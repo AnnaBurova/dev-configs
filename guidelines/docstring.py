616b365ea599c9f9c548d00d89947edd5500fa67
Anna Burova <50639099+AnnaBurova@users.noreply.github.com>
2025-10-16 22:52:00 +0300

    feat: add docstring.py with example function and detailed docstring template

---
+++ guidelines/docstring.py
@@ -0,0 +1,23 @@
+"""
+Created on 2025-10
+
+@author: NewtCode Anna Burova
+"""
+
+def my_function(param1: int, param2: str) -> bool:
+    """
+    Short description of what the function does.
+
+    Longer description (optional, can span multiple lines).
+
+    Parameters:
+        param1 (int): Description of the first parameter.
+        param2 (str): Description of the second parameter.
+
+    Returns:
+        bool: Description of what is returned.
+
+    Raises:
+        ValueError: Description of conditions under which this error is raised.
+    """
+    return True
